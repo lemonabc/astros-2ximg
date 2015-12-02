66850b428df32a1c3ad6fbe83d40042a255925e6
tiger <wanhu88@gmail.com>
2015-12-02 16:57:53 +0800

    Initial commit

---
+++ astro-2ximg.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var path = require('path'),
+    img = require('image-magic').size;
+
+/**
+ * Astro中间件
+ * 
+ * 当访问以"2x"结束的图片（如abc.jpg）时，
+ * 如存在abc2x.jpg，则按原图1/2尺寸存储为abc.jpg，并返回
+ */
+
+module.exports = new astro.Middleware({
+    fileType: 'img'
+}, function(asset, next) {
+    // 发布 abc2x.jpg时，自动发布abc.jpg
+    if (asset.status == 'release' &&
+        (/2x$/i).test(path.basename(asset.path, asset.extname))) {
+
+        let half = asset.clone();
+    
+        half.path = asset.filePath.replace(/2x(\..*?)$/i, function(a, b) {
+            return b
+        });
+        half.name = asset.name.replace(/2x(\..*?)$/i, function(a, b) {
+            return b
+        });
+        img.createImgFrom2xWithCacheSync(half.path,
+            path.join(asset.prjCfg.imgCache, 'img2x.json'));
+
+        // half.debug = 1;
+        half.release(function() {
+            next(asset);
+        });
+        return;
+    }
+    // 访问 abc.jpg，如存在abc2x.jpg，则返回缩放1/2的abc2x.jpg
+    if (img.createImgFrom2xWithCacheSync(asset.filePath,
+            path.join(asset.prjCfg.imgCache, 'img2x.json'))) {
+        asset.data = asset.read();
+    }
+    next(asset);
+});
